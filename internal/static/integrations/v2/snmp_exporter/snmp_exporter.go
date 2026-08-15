60ff8d780f7d440ed96a680959cf710a12f64ff1
bgh <git@bgh.io>
2026-08-15 21:28:38 +1000

    chore(prometheus.exporter.snmp): Update snmp_exporter to v0.30.1

--- internal/static/integrations/v2/snmp_exporter/snmp_exporter.go
+++ internal/static/integrations/v2/snmp_exporter/snmp_exporter.go
@@ -48,7 +48,7 @@ func (c *Config) Identifier(globals integrations_v2.Globals) (string, error) {
 
 // NewIntegration creates a new SNMP integration.
 func (c *Config) NewIntegration(l *slog.Logger, globals integrations_v2.Globals) (integrations_v2.Integration, error) {
-	snmpCfg, err := snmp_exporter.LoadSNMPConfig(c.SnmpConfigFile, &c.SnmpConfig, c.SnmpConfigMergeStrategy)
+	snmpCfg, err := snmp_exporter.LoadSNMPConfig(l, c.SnmpConfigFile, &c.SnmpConfig, c.SnmpConfigMergeStrategy)
 	if err != nil {
 		return nil, err
 	}
